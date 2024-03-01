e9d26a9803d3230b7800ce48d030528d925b734a
VaishnaviEga <141392935+VaishnaviEga@users.noreply.github.com>
2024-03-01 18:05:27 +0530

    Add files via upload

---
+++ ClgDeptFacultyApplication.java
@@ -0,0 +1,13 @@
+package com.example.ClgDeptFaculty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClgDeptFacultyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClgDeptFacultyApplication.class, args);
+	}
+
+}
